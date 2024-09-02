77c1e3e34e1be0e6d517f252f6182da016014708
mateonoel2 <mateo.noel@utec.edu.pe>
2024-09-01 20:40:56 -0500

    a

---
+++ src/main/java/com/example/demo_lab_s3/DemoLabS3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_lab_s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLabS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLabS3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_lab_s3/DemoLabS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_lab_s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLabS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
