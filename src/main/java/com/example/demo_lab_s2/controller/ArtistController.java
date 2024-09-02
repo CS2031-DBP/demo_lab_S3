7a9fbb1a2a26ee1436d9d1c2bb9bee8d30d562a3
mateonoel2 <mateo.noel@utec.edu.pe>
2024-09-02 08:22:38 -0500

    avance clase

--- src/main/java/com/example/demo_lab_s2/controller/ArtistController.java
+++ src/main/java/com/example/demo_lab_s2/controller/ArtistController.java
@@ -1,10 +1,12 @@
 package com.example.demo_lab_s2.controller;
 
 import com.example.demo_lab_s2.domain.Artist;
+import com.example.demo_lab_s2.exceptions.ResourceNotFoundException;
 import com.example.demo_lab_s2.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+//import com.example.demo_lab_s2
 
 import java.util.List;
 
@@ -25,6 +27,14 @@ public class ArtistController {
         List<Artist> allArtists = artistRepository.findAll();
         return ResponseEntity.ok(allArtists);
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Artist> getArtistById(@PathVariable Long id){
+        Artist artist = artistRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Artist with id " + id + " not found"));
+        return ResponseEntity.ok(artist);
+    }
+
 
 
     @GetMapping("/test")
